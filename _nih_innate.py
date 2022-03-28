48f38b2c9b36ddb73bf9bbff633f14e26f252878
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-28 21:17:54 +0000

    datasets

--- _nih_innate.py
+++ _nih_innate.py
@@ -8,7 +8,7 @@ import xarray as xa
 import numpy as np
 import sparse
 from .helpers import config
-from .common.caching import lazy, compose, PickleCache
+from .common.caching import lazy, compose, XArrayCache
 
 #%%
 def _data1():
@@ -18,16 +18,22 @@ def _data1():
     )
     return d
 
-class _dataset1:
+def _data2():
+    d = scanpy.read(
+        config.cache/'data'/'nih-innate.h5ad'
+    )
+    return d
+
+class _nih_innate:
     pass
 
 def _():
-    _dataset1.storage = config.cache/'nih-innate'
+    _nih_innate.storage = config.cache/'nih-innate'
 
     @compose(property, lazy)
     def data(self):
         return _data1()
-    _dataset1.data = data
+    _nih_innate.data = data
 
     @compose(property, lazy)
     def X(self):
@@ -37,7 +43,7 @@ def _():
 
         dtype = self.data.X.dtype
         shape = self.data.shape
-        chunks = (shape[0]//39, shape[1]//1)
+        chunks = (shape[0]//31, shape[1]//1)
         def _(x, n):
             s = x//n
             c = (n,)*s
@@ -50,6 +56,7 @@ def _():
         path = self.storage/'X'
         if not path.exists():
             path.mkdir(parents=True, exist_ok=False)
+            X = _data2().X
         def _(block_info=None):
             bi = block_info[None]
             al = bi['array-location']
@@ -57,18 +64,16 @@ def _():
             if file.exists():
                 x = sparse.load_npz(file)
                 return x
-            data = _data1()
-            x = data.chunk_X(select=np.arange(al[0][0], al[0][1]))
-            x = x[:, slice(*al[1])]
-            x = sparse.COO.from_numpy(x, idx_dtype=np.int32)
+            x = X[slice(*al[0]), slice(*al[1])]
+            x = sparse.COO.from_scipy_sparse(x)
             sparse.save_npz(file, x)
             return x
         x = da.map_blocks(_, chunks=chunks, meta=sparse.zeros((0,0), dtype=dtype))
 
         return x
-    _dataset1.X = X
+    _nih_innate.X = X
 
-    @compose(property, lazy, PickleCache())
+    @compose(property, lazy, XArrayCache())
     def var_ensembl(self):
         from .ensembl import annotations
         display = annotations.gene_display(db='homo_sapiens_core_104_38') 
@@ -85,42 +90,21 @@ def _():
         annot = annot[annot['var'].isin(self.data.var_names)]
         annot = annot.set_index('var').to_xarray()
         return annot
-    _dataset1.var_ensembl = var_ensembl
-
-    @compose(property, lazy, PickleCache())
-    def sigs(self):
-        from .msigdb import sigs
-        e = self.var_ensembl[['entrez']].to_dataframe().reset_index()
-        e['e'] = 1
-        e = e.drop_duplicates()
-        e['entrez'] = e.entrez.astype(int)
-        e = e.set_index(['var', 'entrez']).e
-        e = xa.DataArray.from_series(e, sparse=True)
-        e = e.fillna(0)
-        e.data = sparse.GCXS(e.data, compressed_axes=[1])
-
-        s = sigs.db.db.copy().astype(int)
-        s.data = sparse.GCXS(s.data, compressed_axes=[1])
-        s = xa.apply_ufunc(
-            np.matmul, e, s,
-            input_core_dims=[['var', 'entrez'], ['entrez', 'sig']],
-            output_core_dims=[['var', 'sig']],
-            join='inner'
-        )
-        s.data = sparse.GCXS(s.data, compressed_axes=[0])
-        s = (s>0).astype(np.float64)
-        return s
-    _dataset1.sigs = sigs
+    _nih_innate.var_ensembl = var_ensembl
 _()
 
-dataset1 = _dataset1()
+nih_innate = _nih_innate()
 
 #%%
 if __name__=='__main__':
-    self = dataset1
+    self = nih_innate
+
+#%%
     import sparse
     x1 = self.X.map_blocks(
         lambda x: x.sum(keepdims=True), 
         meta=sparse.zeros((0,0), dtype=self.X.dtype)
     )
     x1 = x1.compute()
+
+# %%
