bd6bb4eb6f2c57174dc5fb45540a874a86e3eef6
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-28 18:12:09 +0000

    env

---
+++ reqs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#conda create -p ./env
+#conda activate ./env
+conda install python==3.7
+conda install ipykernel
+conda install zarr
+conda install xarray
+conda install scanpy
+conda install plotnine
+conda install sparse
+conda install dask
