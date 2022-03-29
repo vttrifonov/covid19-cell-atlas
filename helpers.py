23f96cb1b9f35361521d9f4db528637ac25cd208
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-29 00:46:17 +0000

    helpers

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 #%%
 from pathlib import Path
+import xarray as xa
 
 #%%
 class _config:
@@ -8,3 +9,17 @@ class _config:
     root = Path(__file__).parent
 
 config = _config()
+
+def xa_dense(x):
+    if isinstance(x, xa.DataArray):
+        if hasattr(x.data, 'todense'):
+            x = x.copy()
+            x.data = x.data.todense()
+    else:
+        for k in x:
+            x[k] = xa_dense(x[k])
+    return x
+
+
+xa.DataArray.todense = xa_dense
+xa.Dataset.todense = xa_dense
