b95d91216f8079672c5467f35908428b1a8ad251
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-29 17:54:18 +0000

    nih

--- _nih_innate.py
+++ _nih_innate.py
@@ -36,11 +36,11 @@ def _():
     _nih_innate.data = data
 
     @compose(property, lazy, SparseCache())
-    def X1(self):
+    def X(self):
         x = _data2().X
         x = sparse.COO.from_scipy_sparse(x)
         return x
-    _nih_innate.X1 = X1
+    _nih_innate.X = X
 
     @compose(property, lazy, XArrayCache())
     def var_ensembl(self):
@@ -60,6 +60,38 @@ def _():
         annot = annot.set_index('var').to_xarray()
         return annot
     _nih_innate.var_ensembl = var_ensembl
+
+    @compose(property, lazy, XArrayCache())
+    def obs(self):
+        o = self.data.obs.copy()
+
+        o['sample'] = o.donor.astype(str) + '_' + o.timepoint.astype(str)
+        
+        _ = o.dsm_severity_score.astype(str)
+        _ = _.where(_!='', 'nan')
+        o['dsm_severity_score'] = _.astype(np.float32)
+
+        samples = o.drop(columns=[
+            'cell_type_ontology_term_id', 
+            'cell_type'
+        ]).drop_duplicates()
+        donors = samples.drop(columns=[
+            'sample', 'timepoint',
+            'days_since_onset', 'days_since_hospitalized'
+        ]).drop_duplicates().set_index('donor').to_xarray()
+        samples = samples[[
+            'sample', 'donor', 'timepoint',
+            'days_since_onset', 'days_since_hospitalized'
+        ]].rename(columns={'donor': 'DonorID'}).\
+            set_index('sample').to_xarray()
+        cells = o[['sample']].\
+            rename(columns={'sample': 'SampleID'}).\
+            to_xarray().rename(index='cell')
+
+        r = xa.merge([samples, donors, cells])
+
+        return r
+    _nih_innate.obs = obs
 _()
 
 nih_innate = _nih_innate()
