4d300d176d3ec6cb0ad203b0ec12bd79367d2069
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-29 00:46:25 +0000

    env

--- reqs.sh
+++ reqs.sh
@@ -10,3 +10,7 @@ conda install scanpy
 conda install plotnine
 conda install sparse
 conda install dask==2021.10.0
+conda install mysql-connector-python
+
+pip install rpy2
+pip install --ignore-installe cffi #this might conflict with scanpy
