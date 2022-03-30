40b0ee4f5c84f06449db668ac02ddbb7ddde0e88
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-30 20:41:48 +0000

    work

---
+++ test_dask.py
@@ -0,0 +1,14 @@
+#%%
+from dask.distributed import LocalCluster, Client
+from dask import delayed, compute
+
+#%%
+if __name__ == '__main__':
+    cluster = LocalCluster(n_workers=2)
+    client = Client(cluster)
+
+    f = compute(delayed(lambda i:i)(i) for i in range(2))
+
+    client.close()
+    cluster.close()
+# %%
