e373ed6f39a138c3ba380a134161484d5516b27e
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-30 19:57:22 +0000

    app

--- app1.R
+++ app1.R
@@ -10,7 +10,7 @@ ui <- {
   fluidPage(
     fluidRow(
         selectizeInput(
-            'genes',
+            'cytokines',
             'Select cytokines',
             choices=NULL,
             multiple=TRUE
@@ -22,7 +22,7 @@ ui <- {
         )
     ),
     fluidRow(
-        DTOutput('genes1')
+        DTOutput('genes')
     ),
     fluidRow(
         column(8,
@@ -40,7 +40,7 @@ ui <- {
 server <- function(input, output, session) {
     {
         if (FALSE) {
-            input <- list(genes=c('IL-6'), genes1=c('IL6'))
+            input <- list(cytokines=c('IL-6'), genes=c('IL6'))
         }
 
         py_run_string('from covid19_cell_atlas._analysis3 import analysis3 as analysis')
@@ -51,12 +51,12 @@ server <- function(input, output, session) {
             x2 <- ceiling(x*10^(n-x1))/10^n
             x2*10^x1
         }
-        genes1 <- py_eval('analysis.fit1.to_dataframe().reset_index()') %>%
+
+        genes <- py_eval('analysis.fit1.to_dataframe().reset_index()') %>%
                 py_to_r() %>%
                 data.table
-
-        genes1 <- genes1[!is.na(`Pr(>F)`)]
-        genes1 <- genes1[, list(
+        genes <- genes[!is.na(`Pr(>F)`)]
+        genes <- genes[, list(
             subset, gene,
             `Pr(>F)`, q, F,
             Intercept,
@@ -64,31 +64,30 @@ server <- function(input, output, session) {
             `dsm_severity_score_group[T.DSM_low]`,
             `days_since_onset:dsm_severity_score_group[T.DSM_low]` 
         )]
-        genes1[, `Pr(>F)` := round_mantisa(`Pr(>F)`, 1)]
-        genes1[, q := round_mantisa(q, 1)]
-        for(c in setdiff(names(genes1), c('q', 'Pr(>F)', 'subset', 'gene'))) {
-            genes1[[c]] <- round(genes1[[c]], 2)
+        genes[, `Pr(>F)` := round_mantisa(`Pr(>F)`, 1)]
+        genes[, q := round_mantisa(q, 1)]
+        for(c in setdiff(names(genes), c('q', 'Pr(>F)', 'subset', 'gene'))) {
+            genes[[c]] <- round(genes[[c]], 2)
         }
 
-
-
         plot1 <- reactive({
-            genes <- req(input$genes)
-            data <- analysis$app1_plot1_data(genes) %>% data.table
+            cytokines <- req(input$cytokines)
+            data <- analysis$app1_plot1_data(cytokines) %>% data.table
+            data <- data[days_since_onset<=40]
             ggplot(data)+
                 aes(x=days_since_onset, y=log2(level))+
                 geom_line(aes(group=donor), alpha=0.1)+
-                geom_point(aes(fill=DSM_group), alpha=0.5, size=2, pch=21)+
-                geom_smooth(aes(color=DSM_group), alpha=0.5, formula='y~x', method='lm')+
+                geom_point(aes(fill=dsm_severity_score_group), alpha=0.5, size=2, pch=21)+
+                geom_smooth(aes(color=dsm_severity_score_group), alpha=0.5, formula='y~x', method='lm')+
                 facet_grid(cytokine~., scales='free')+
                 labs(y='log2(pg/mL)')
         })
 
 
         plot2 <- reactive({
-            rows <- req(input$genes1_rows_selected)
-            genes <- genes1$gene[rows]
-            data <- analysis$app1_plot2_data(genes) %>% data.table
+            rows <- req(input$genes_rows_selected)
+            gene <- genes$gene[rows]
+            data <- analysis$app1_plot2_data(gene) %>% data.table
 
             ggplot(data)+
                 aes(days_since_onset, y=log2(X+1))+
@@ -100,9 +99,9 @@ server <- function(input, output, session) {
         })
 
         plot3 <- reactive({
-            row <- req(input$genes1_rows_selected)
-            genes <- genes1$gene[row]
-            data <- analysis$app1_plot3_data(genes) %>% data.table
+            row <- req(input$genes_rows_selected)
+            gene <- genes$gene[row]
+            data <- analysis$app1_plot3_data(gene) %>% data.table
 
             ggplot(data)+
                 aes(days_since_onset, y=log(X+1))+
@@ -116,20 +115,20 @@ server <- function(input, output, session) {
 
     {
         updateSelectizeInput(
-            session, 'genes',
-            choices = analysis$app1_genes,
+            session, 'cytokines',
+            choices = analysis$app1_cytokines,
             server = TRUE
         )
 
         output$plot1.ui <- renderUI({
-            genes <- req(input$genes)
-            plotOutput('plot1', height=200*length(genes))
+            cytokines <- req(input$cytokines)
+            plotOutput('plot1', height=200*length(cytokines))
         })
 
         output$plot1 <- renderPlot(plot1())
 
-        output$genes1 <- renderDT(
-            genes1 %>%
+        output$genes <- renderDT(
+            genes %>%
                 datatable_scroller(
                     filter = 'top', selection = 'single',
                     options = list(dom = 't')
