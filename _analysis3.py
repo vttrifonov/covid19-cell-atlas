6efc5ae65a4d78f8d0e033e8898bfc6a9cd07b7c
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-03-30 19:57:19 +0000

    analysis

--- _analysis3.py
+++ _analysis3.py
@@ -28,16 +28,46 @@ from ._analysis2 import analysis2
 class _analysis3:
     storage = Path(config.cache)/'analysis3'
 
-    @property
-    def metadata(self):
-        return paper.metadata
-    
     @compose(property, lazy)
     def cytokines(self):
-        return paper.cytokines
+        x1 = paper.cytokines
+        x1 = x1.to_dataframe().reset_index(drop=True)
+        x1['sample'] = x1['DonorID'] + '_' + x1['days_from_symptom_onset_to_test'].astype(str)
+        x1 = xa.merge([
+           x1[['level', 'cytokine', 'sample']].set_index(['cytokine', 'sample']).to_xarray(),
+           x1[['sample', 'DonorID', 'days_from_symptom_onset_to_test']].\
+               drop_duplicates().\
+               set_index('sample').to_xarray(),
+        ])
+
+        x2 = self.pseudobulk.drop_dims(['cell_type', 'gene'])
+        x2 = x2.drop([
+            'tot',
+            'days_since_onset', 'days_since_hospitalized',
+            'timepoint', 'subset',
+            'assay', 'assay_ontology_term_id', 'material_type',
+            'assay_ontology_term_id', 'tissue', 
+            'tissue_ontology_term_id',
+        ])
+        x2 = x2.to_dataframe().reset_index(drop=True).drop_duplicates()
+        x2 = x2.merge(
+            x1[['DonorID']].to_dataframe().reset_index().\
+                drop_duplicates(),
+            left_on='donor', right_on='DonorID'
+        ).drop(columns=['donor']).\
+            set_index('sample').to_xarray()
         
-    @property
-    def obs(self):
+        x1 = xa.merge([x1, x2], join='inner')
+
+        x1 = x1.rename(
+            days_from_symptom_onset_to_test='days_since_onset',
+            DonorID='donor'
+        )
+
+        return x1
+        
+    @compose(property, lazy)
+    def pseudobulk(self):
         o1 = analysis1.obs.copy()
         o1['sample'] = o1['sample']+'_innate'
         o1['subset'] = ('sample', ['innate']*o1.sizes['sample'])
@@ -66,10 +96,7 @@ class _analysis3:
         x4 = x3.merge(x2, on='DonorID')
         x4 = x4.set_index('sample').to_xarray()
         x4 = x4.rename(DonorID='donor')
-        return x4
 
-    @property
-    def X(self):
         x1 = analysis1.X2.copy()
         x2 = analysis2.X2.copy()
 
@@ -79,65 +106,27 @@ class _analysis3:
 
         x['tot'] = x.X.sum(dim=['cell_type', 'gene'])
         x['X'] = 1e6*x.X/x.tot
-        return x
 
-    def data2(self, gene):
-        x2 = self.obs[['severity', 'outcome', 'dsm_severity_score', 'dsm_severity_score_group']].to_dataframe()
-        x3 = np.where(
-            x2.severity=='', 'Control',
-            x2.severity+'-'+x2.outcome
-        )
-        x3 = pd.Categorical(
-            x3,
-            categories=[
-                'Control', 'Moderate-alive', 'Severe-alive', 
-                'Critical-alive', 'Critical-deceased'
-            ]
-        )        
-        x2['status'] = x3
+        r = xa.merge([x, x4], join='inner')
 
-        x3 = self.obs[['days_since_onset', 'DonorID', 'timepoint', 'subset']]
-        x3 = x3.to_dataframe().reset_index()
-        x3 = x3.rename(columns={'DonorID': 'donor'})
+        return r
 
-        _ = x3.days_since_onset
-        _ = _.where(_!='', 'nan').astype(np.float32)
-        x3['days_since_onset'] = _
-        
-        x1 = self.X2.X
-        x1 = 1e6*x1/x1.sum(dim=['cell_type', 'gene'])
-        x1 = x1.sel(gene=gene)
-        x1 = x1.to_dataframe().reset_index()
-        x1 = x1[~x1.X.isna()]
-        x1 = x1.merge(x3, on='sample')
-        x1 = x1.merge(x2, on='donor')
-        return x1
+    @property
+    def app1_cytokines(self):
+        return self.cytokines.cytokine.data
 
     @property
     def app1_genes(self):
-        x1 = self.cytokines
-        return x1.cytokine.to_series().drop_duplicates().to_list()
+        return self.pseudobulk.gene.data
 
-    @property
-    def app1_genes1(self):
-        return self.X.gene.data
-
-    def app1_plot1_data(self, gene):
-        x1 = self.cytokines
-        x1 = x1.sel(subject_test_day=x1.cytokine.isin([gene]))
-        x1 = x1.to_dataframe().reset_index(drop=True)
-
-        x2 = self.metadata.DSM_group.to_dataframe().reset_index()
-
-        x3 = x1.merge(x2, left_on='DonorID', right_on='donor')
-        del x3['DonorID']
-        x3 = x3.rename(columns={'days_from_symptom_onset_to_test': 'days_since_onset'})
-        x3 = x3[~x3.DSM_group.isna()]
-        x3 = x3[x3.days_since_onset<=40]        
-        return x3
+    def app1_plot1_data(self, cytokine):
+        x1 = self.cytokines.sel(cytokine=cytokine)
+        x1 = x1.sel(sample=x1.dsm_severity_score_group!='')
+        x1 = x1.to_dataframe().reset_index()
+        return x1
 
     def app1_plot2_data(self, gene):
-        x1 = xa.merge([self.X.sel(gene=gene), self.obs])
+        x1 = self.pseudobulk.sel(gene=gene)
         x1 = x1.sel(sample=x1.dsm_severity_score_group!='')
         x1 = x1.sum(dim='cell_type')
         x1 = x1.to_dataframe().reset_index()
@@ -151,9 +140,8 @@ class _analysis3:
         x1['status'] = x3
         return x1
 
-
     def app1_plot3_data(self, gene):
-        x1 = xa.merge([self.X.sel(gene=gene), self.obs])
+        x1 = self.pseudobulk.sel(gene=gene)
         x1 = x1.sel(sample=x1.dsm_severity_score_group!='')
         x1 = x1.to_dataframe().reset_index()
         x3 = pd.Categorical(
@@ -168,7 +156,7 @@ class _analysis3:
 
     @compose(property, lazy, XArrayCache())
     def fit1(self):
-        x1 = xa.merge([self.X, self.obs])
+        x1 = self.pseudobulk
         x1 = x1[['X', 'days_since_onset', 'dsm_severity_score_group', 'subset']]
         x1 = x1.sum(dim='cell_type')
         x1['X'] = np.log1p(x1.X)/np.log(2)
@@ -201,6 +189,29 @@ class _analysis3:
         x8 = x8.set_index(['subset', 'gene']).to_xarray()
         return x8
 
+
+    @compose(property, lazy, XArrayCache())
+    def fit2(self):
+        x1 = self.cytokines
+        x1 = x1[['level', 'days_since_onset', 'dsm_severity_score_group']]
+        x1['level'] = np.log1p(x1.level)/np.log(2)
+        x1 = x1.to_dataframe().reset_index()
+        x1 = x1[x1.dsm_severity_score_group!='']
+
+        def f1(x):
+            x3 = smf.ols('level~days_since_onset', data=x).fit()
+            x4 = smf.ols('level~days_since_onset * dsm_severity_score_group', data=x).fit()
+            x5 = anova_lm(x3, x4).loc[[1],:].reset_index(drop=True)
+            x5 = pd.concat([x5, pd.DataFrame(x4.params).T], axis=1)
+            return x5
+        
+        x8 = x1.groupby(['cytokine']).apply(f1)
+        x8 = x8.reset_index().drop(columns='level_1')
+        x9 = ~x8['Pr(>F)'].isna()
+        x8.loc[x9, 'q'] = multipletests(x8[x9]['Pr(>F)'], method='fdr_bh')[1]
+        x8 = x8.set_index(['cytokine']).to_xarray()
+        return x8
+
 analysis3 = _analysis3()
 
 #%%
@@ -210,11 +221,12 @@ if __name__ == '__main__':
 
 #%%
     x3 = self.app1_plot1_data(['IL-6'])
+    x3 = x3[x3.days_since_onset<=40]
     print(        
-        ggplot(x3)+aes('days_since_onset', 'np.log1p(level)/np.log(10)')+
-            geom_point(aes(fill='DSM_group'), alpha=0.5)+
+        ggplot(x3)+aes('days_since_onset', 'np.log1p(level)/np.log(2)')+
+            geom_point(aes(fill='dsm_severity_score_group'), alpha=0.5)+
             geom_line(aes(group='donor'), alpha=0.1)+
-            geom_smooth(aes(color='DSM_group'), alpha=0.5, method='lm')+
+            geom_smooth(aes(color='dsm_severity_score_group'), alpha=0.5, method='lm')+
             facet_grid('cytokine~.', scales='free')+
             theme(figure_size=(6, 4))+
             labs(y='log2(pg/mL)')
@@ -233,19 +245,19 @@ if __name__ == '__main__':
     )
 
 #%%
-    x2 = self.app1_plot3_data('IL18BP')
+    x2 = self.app1_plot3_data('IL6')
     print(        
         ggplot(x2)+aes('days_since_onset', 'np.log1p(X)/np.log(2)')+
             geom_point(aes(fill='dsm_severity_score_group'), alpha=0.5)+
             geom_line(aes(group='donor'), alpha=0.1)+
             geom_smooth(aes(color='dsm_severity_score_group'), alpha=0.5)+
-            facet_grid('cell_type+gene~.', scales='free_x')+
+            facet_grid('subset+cell_type+gene~.', scales='free_x')+
             theme(figure_size=(4, 30))+
             labs(y='log2RPM')
     )
 
 #%%
-    x2 = x1[['donor', 'status', 'dsm_severity_score', 'dsm_severity_score_group']].drop_duplicates()
+    x2 = self.app1_plot2_data('IL6')
     print(
         ggplot(x2)+aes('status', 'dsm_severity_score')+
             geom_violin(aes(fill='status'))+
@@ -253,3 +265,5 @@ if __name__ == '__main__':
             geom_point(aes(color='dsm_severity_score_group'))+
             theme(axis_text_x=element_text(angle=45, ha='right'))
     )
+
+# %%
