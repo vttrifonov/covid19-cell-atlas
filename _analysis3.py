8bccba057213e6430093a2c314d6bbc3eb6ba57e
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-04-13 04:36:27 +0000

    analysis

--- _analysis3.py
+++ _analysis3.py
@@ -224,35 +224,24 @@ class _analysis3:
 analysis3 = _analysis3()
 
 #%%
-if __name__ == '__main__':
-    self = analysis3
 
-# %%
-    import statsmodels.api as sm
+@compose(property, lazy, XArrayCache())
+def fit_time1(self):
+#%%    
     import statsmodels.formula.api as smf
     from statsmodels.stats.anova import anova_lm
     from .sigs.fit import multipletests
-    from ._helpers import dataarray_from_series
-    from sklearn.decomposition import PCA
-    from plotnine import *
-    from scipy import stats
 
     x1 = self.cytokines
-    x1 = x1[['level', 'days_since_onset', 'dsm_severity_score', 'dsm_severity_score_group', 'status', 'donor']]
-    #x1 = x1.sel(sample=x1.dsm_severity_score_group=='DSM_high')
     x1 = x1.sel(cytokine=~x1.cytokine.isin(['IFN-alpha2a']))
+    x1 = x1[['level', 'days_since_onset', 'donor']]
     x1['level'] = np.log1p(x1.level)/np.log(2)
     x1 = x1.to_dataframe().reset_index()
-    x1 = x1[x1.dsm_severity_score_group!='']
-    x1['time1'] = x1.days_since_onset
+    x1 = x1[~x1['level'].isna()]
 
-#%%
     def f1(x):
-        x = x[~x['level'].isna()].copy()
-        for d in range(2,4):
-            x[f'level_{d}'] = x['level']**d
-        x3 = smf.ols('time1~1', data=x).fit()
-        x4 = smf.ols('time1~level', data=x).fit()
+        x3 = smf.ols('days_since_onset~1', data=x).fit()
+        x4 = smf.ols('days_since_onset~level', data=x).fit()
         x5 = anova_lm(x3, x4).loc[[1],:].reset_index(drop=True)
         x5 = pd.concat([x5, pd.DataFrame(x4.params).T], axis=1)
         x5['rsq'] = x5.ss_diff/(x5.ss_diff+x5.ssr)
@@ -264,45 +253,36 @@ if __name__ == '__main__':
     x8 = x8.set_index(['cytokine']).to_xarray()
 
 #%%
-    x9 = x8.to_dataframe().reset_index()
+    return x8
+_analysis3.fit_time1 = fit_time1
+
+@compose(property, lazy, XArrayCache())
+def fit_time2(self):
+    import statsmodels.api as sm
+    from sklearn.decomposition import PCA
+    from statsmodels.stats.anova import anova_lm
+
+    x9 = self.fit_time1
+    x9 = x9.to_dataframe().reset_index()
     x9 = x9.sort_values('Pr(>F)')
     x9 = x9[x9.q<0.05]
-    x9
 
-#%%
-    for x11 in x9.itertuples():
-        x10 = x1[x1.cytokine==x11.cytokine][['time1', 'level']]
-        x10 = x10[~x10.level.isna()]
-        x12 = pd.DataFrame(dict(
-            level=np.linspace(x10.level.min(), x10.level.max(), num=100)
-        ))
-        x12['time1'] = x11.Intercept+x11.level*x12['level']#+x11.level_2*x12['level']**2+x11.level_3*x12['level']**3
-        print(
-            ggplot()+aes('level', 'time1')+
-                geom_point(data=x10)+
-                geom_line(data=x12)+
-                labs(title=x11.cytokine)
-        )
+    x1 = self.cytokines
+    x1 = x1[['level', 'days_since_onset', 'donor']]
+    x1 = x1.sel(cytokine=x9.cytokine.to_list())
+    x1['level'] = np.log1p(x1.level)/np.log(2)
+    x1 = x1.to_dataframe().reset_index()
+    x1 = x1[~x1['level'].isna()]
 
-#%%
-    x2 = x1[x1.cytokine.isin(x9.cytokine)].copy()
-    x2 = x2.groupby(['donor', 'time1', 'cytokine', 'dsm_severity_score', 'dsm_severity_score_group', 'status'])['level'].mean()
-    x2 = x2.reset_index()
-
-    x5 = x2.copy()
-    x5 = x5.pivot_table(index=['time1', 'donor', 'dsm_severity_score', 'dsm_severity_score_group', 'status'], columns='cytokine', values='level')
-    x6 = "Q('"+x5.columns+"')"
-    x6 = '+'.join(x6)
+    x5 = x1.groupby(['donor', 'days_since_onset', 'cytokine'])['level'].mean()
+    x5 = x5.reset_index()
+    x5 = x5.pivot_table(index=['days_since_onset', 'donor'], columns='cytokine', values='level')
     x10 = x5.to_numpy()
     x18 = x5.columns
     x5 = x5.reset_index()
     x11 = np.isnan(x10).sum(axis=1)
     x5 = x5[x11==0]
     x10 = x10[x11==0,:]
-    x10 = np.c_[x10]#, x10**2, x10**3]
-
-    #x7 = smf.ols(f'time1~{x6}', data=x5).fit()
-    #print(x7.summary())
 
     x12 = PCA(n_components=x10.shape[1]).fit(x10)
     x13 = x12.transform(x10)
@@ -313,48 +293,104 @@ if __name__ == '__main__':
             np.c_[[0]*x12.n_components, x12.components_]
         ]
     ]
-    #print(np.quantile(x13[0] @ x13[1] - sm.add_constant(x10), [0, 0.5, 1]))
-    x13[1] = np.linalg.inv(x13[1].T @ x13[1]) @ x13[1].T
+    x13[1] = np.linalg.pinv(x13[1])
     
     x14 = [
-        sm.OLS(x5.time1.to_numpy(), x13[0][:,:i]).fit()
-        for i in range(2, x13[0].shape[1]+1)
+        sm.OLS(x5.days_since_onset.to_numpy(), x13[0][:,:i]).fit()
+        for i in range(1, x13[0].shape[1]+1)
     ]
-    #print(x14[-1].summary())
-    x14 = sorted(x14, key=lambda x: x.f_pvalue)
-    print(x14[0].summary())
+    x14_1 = x14[0]
+    x14 = sorted(x14[1:], key=lambda x: x.f_pvalue)[0]
 
-    x15 = x13[1][:,:len(x14[0].params)]@x14[0].params    
+    x15 = x13[1][:,:len(x14.params)]@x14.params
+    x15 = pd.Series(x15, index=['const'] + x18.to_list())
+    x16 = anova_lm(x14_1, x14).loc[[1],:].reset_index(drop=True)
+    x16['rsq'] = x16.ss_diff/(x16.ss_diff+x16.ssr)
+    x16 = x16.T[0]
 
-    print(pd.DataFrame(dict(
-        coef=['const'] + x18.to_list(),
-        value=x15
-    )))
+    x17 = xa.merge([
+        x15.to_xarray().rename(index='coef').rename('coef_value'),
+        x16.to_xarray().rename(index='stat').rename('stat_value')
+    ])
 
-    x5['time2'] = sm.add_constant(x10)@x15
+    return x17
+_analysis3.fit_time2 = fit_time2
+
+
+#%%
+if __name__ == '__main__':
+    self = analysis3
+
+# %%
+    from plotnine import *
+
+# %%
+    x9 = self.fit_time1
+    x9 = x9.to_dataframe().reset_index()
+    x9 = x9.sort_values('Pr(>F)')
+    x9 = x9[x9.q<0.05]
+
+    x15 = self.fit_time2
+
+    x1 = self.cytokines
+    x1 = x1.sel(cytokine=x9.cytokine.to_list())
+    x1 = x1[['level', 'days_since_onset', 'donor', 'status', 'dsm_severity_score', 'dsm_severity_score_group']]
+    x1['level'] = np.log1p(x1.level)/np.log(2)
+    x1 = x1.to_dataframe().reset_index()
+    x1 = x1[~x1['level'].isna()]
+
+    x5 = x1.groupby(['donor', 'days_since_onset', 'cytokine', 'status', 'dsm_severity_score', 'dsm_severity_score_group'])['level'].mean()
+    x5 = x5.reset_index()
+    x5 = x5.pivot_table(index=['days_since_onset', 'donor', 'status', 'dsm_severity_score', 'dsm_severity_score_group'], columns='cytokine', values='level')
+    x5 = x5.reset_index()
+    x5['const'] = 1
+    x5['days_since_onset_pred'] = x5[x15.coef.data].to_numpy() @ x15.coef_value.data
+    x5['delta_days'] = x5.days_since_onset_pred-x5.days_since_onset
+    x5 = x5[~x5.delta_days.isna()]
+
+#%%
+    for x11 in x9.itertuples():
+        x10 = x1[x1.cytokine==x11.cytokine][['days_since_onset', 'level']]
+        x12 = pd.DataFrame(dict(
+            level=[x10.level.min(), x10.level.max()]
+        ))
+        x12['days_since_onset'] = x11.Intercept+x11.level*x12['level']
+        print(
+            ggplot()+aes('level', 'days_since_onset')+
+                geom_point(data=x10)+
+                geom_line(data=x12)+
+                labs(title=x11.cytokine)
+        )
+
+#%%
+    x6 = x15.stat_value.to_series()
+    print(np.sqrt(x6.ssr/x6.df_resid))
+
+#%%    
     print(
-        ggplot(x5)+aes('time1', 'time2', color='status')+
+        ggplot(x5)+aes('days_since_onset', 'days_since_onset_pred', color='status')+
             geom_point()+
             geom_abline(slope=1, intercept=0)
     )
 
+#%%
     print(
-        ggplot(x5)+aes('status', 'time2-time1')+
+        ggplot(x5)+aes('status', 'delta_days')+
             geom_boxplot()+
             geom_point()
     )
 
+#%%
     print(
-        ggplot(x5)+aes('dsm_severity_score', 'time2-time1')+
+        ggplot(x5)+aes('dsm_severity_score', 'delta_days', color='dsm_severity_score_group')+
             geom_point()+geom_smooth(method='lm')
     )
 
-    print(np.sqrt(np.mean((x5.time1-x5.time2)**2)))
-
-    x5['delta_time'] = x5.time2-x5.time1
+#%%
+    from scipy import stats
     stats.ttest_ind(
-        x5.delta_time[x5.dsm_severity_score_group=='DSM_high'],
-        x5.delta_time[x5.dsm_severity_score_group=='DSM_low']
+        x5.delta_days[x5.dsm_severity_score_group=='DSM_high'],
+        x5.delta_days[x5.dsm_severity_score_group=='DSM_low']
     )
 
 # %%
